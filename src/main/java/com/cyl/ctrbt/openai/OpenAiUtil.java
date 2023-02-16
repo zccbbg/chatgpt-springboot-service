b93c9a01f653acacf3b832c4fa2c3b8738133536
zhaochencheng <zccbbg@qq.com>
2023-02-16 13:18:24 +0800

    延长请求时间，修复老是报timeout的错误

--- src/main/java/com/cyl/ctrbt/openai/OpenAiUtil.java
+++ src/main/java/com/cyl/ctrbt/openai/OpenAiUtil.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -20,12 +21,12 @@ public class OpenAiUtil {
 
     @PostConstruct
     public void init(){
-        service= new OpenAiService(token);
+        service= new OpenAiService(token, Duration.ofSeconds(60l));
     }
     public List<CompletionChoice> sendComplete(String prompt) {
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .model("text-davinci-003")
-                .maxTokens(150)
+                .maxTokens(1500)
                 .prompt(prompt)
                 .user("testing")
                 .logitBias(new HashMap<>())
