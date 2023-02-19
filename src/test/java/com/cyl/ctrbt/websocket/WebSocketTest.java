73015c418b5eddee70566573353df6ecc505029d
zhaochencheng <zccbbg@qq.com>
2023-02-19 12:38:55 +0800

    增加一些项目说明

--- src/test/java/com/cyl/ctrbt/websocket/WebSocketTest.java
+++ src/test/java/com/cyl/ctrbt/websocket/WebSocketTest.java
@@ -12,32 +12,21 @@ public class WebSocketTest {
 
     @Test
     public void test() {
-        URI uri = URI.create("ws://127.0.0.1:8080/websocket");  //注意协议号为ws
+        URI uri = URI.create("ws://127.0.0.1:8081/websocket");  //注意协议号为ws
         MyWebSocketClient client = new MyWebSocketClient(uri);
         client.addHeader("token", "token-123456");              //这里为header添加了token，实现简单的校验
         try {
             client.connectBlocking();   //在连接成功之前会一直阻塞
 
-            Timer timer = new Timer();
-            MyTimerTask timerTask = new MyTimerTask(client);
-            timer.schedule(timerTask, 1000L, 2000L);
-            Thread.sleep(120000);
+            while (true) {
+                if (client.isOpen()) {
+                    client.send("先有鸡还是先有蛋？");
+                }
+                Thread.sleep(1000000);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    static class MyTimerTask extends TimerTask {
-
-        private final MyWebSocketClient client;
-
-        public MyTimerTask(MyWebSocketClient client) {
-            this.client = client;
-        }
-
-        @Override
-        public void run() {
-            client.send("Test message from client, the number is " + count.getAndIncrement());
-        }
-    }
 }
